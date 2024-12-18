f64dd6c197e04d8f8fcecf6e644ebb58ccb7d12b
konradkjems <konrad@tapfeed.dk>
2024-12-18 19:48:42 +0100

    Second commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -68,7 +68,7 @@ const Login = () => {
     setIsLoading(true);
     
     try {
-        const response = await fetch('http://localhost:3000/api/auth/login', {
+        const response = await fetch('https://api.tapfeed.dk/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
